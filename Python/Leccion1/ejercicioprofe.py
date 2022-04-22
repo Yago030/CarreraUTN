70aebab0c5b8a00cae0b77d8958f7fbf4eeac03f
Santiago Bergerat <santiagobergerat@gmail.com>
2022-04-21 21:47:02 -0300

    Primer commit global carpeta tecnicatura

---
+++ Python/Leccion1/ejercicioprofe.py
@@ -0,0 +1,4 @@
+autor =input("ingrese el nombre del autor")
+titulo=input("ingrese el ittulo del libre")
+
+print("El autor es ",autor," del titulo ", titulo)
---
+++ Python/Leccion1/main.py
@@ -0,0 +1,16 @@
+
+var1 =3
+
+var=5
+
+print("la suma es igual a : "+ "var1")
+
+print(id(var))
+
+# asi comentamos en python
+resultado=input("digite un numero: ")
+print(resultado)
+
+#convertimos de la entrada de datos
+
+numero1=int(input("ingrese el numero que sera convetido a int "))
